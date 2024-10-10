3980fdacc73b42f8676178e3de4d0f13ff3c65df
YO <keneth.javier90@gmail.com>
2024-10-10 10:58:42 -0600

    se areglan erores en el componente 1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { navBarDiv } from "./componente1/componente.js";
 
 let DOM = document.querySelector("#root");
 
-let navBar = document.createElement('section');
+let navBar = document.createElement('header');
 navBar.className = "nav-bar";
 navBar.appendChild(navBarDiv);
 DOM.appendChild(navBar);
